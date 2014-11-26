1eef817b9d06588fb5c6e412d07c1251976c892f
quantizs <quantizs@239338c0-6480-a7f8-7df0-83dcd0cb3393>
2014-11-26 14:55:13 +0000

    Kommentkben ötletek

--- Research/Sketch_Code/HumanDetector/example/mainwindow.cpp
+++ Research/Sketch_Code/HumanDetector/example/mainwindow.cpp
@@ -55,6 +55,22 @@ void MainWindow::on_pushButton_clicked()
         extractor = new ObjectExtraction();
 
         connect(extractor,SIGNAL(SendFrame(QImage)), this, SLOT(ReciveFrame(QImage)));
+        //Kellene egy frame manager, ami csak frameket rögzít (esetleg előfeldolgozást végez - szürke skálázás)
+        //Ezt a framemanagert kellene összekapcsolni egy szignállal a detektálással, követéssel és megjelenítéssel
+        //Illetve külön a detektálás -> (Template) -> Követés -> (Rect) -> Megjelenítés összekapcsolása
+        //A megjelenítés már csak rá rajzolja a framemanager-től kapott frame-re a követéstől kapott rectet.
+		
+		//Jó ötletnek tűnik:
+		//A template-et követés közben is lehet frissíteni a templatet, a korábbi template alapján megtalált helyen látott minta alapján. 
+		
+		//Egyéb template maching használathoz:
+		//https://gist.github.com/yoggy/1451097
+		//http://stackoverflow.com/revisions/20180073/3
+		//http://docs.opencv.org/modules/imgproc/doc/object_detection.html
+		//http://opencv-code.com/tutorials/fast-template-matching-with-image-pyramid/
+		//http://samwize.com/2013/06/09/using-opencv-to-match-template-multiple-times/
+		//http://stackoverflow.com/questions/20180073/real-time-template-matching-opencv-c
+		//http://docs.opencv.org/trunk/doc/py_tutorials/py_imgproc/py_template_matching/py_template_matching.html
 
         extractor->Init(detectorXML, cameraID);
         extractor->start();
